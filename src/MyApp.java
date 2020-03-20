3068ddfc63ed661a7b4c01df2ce99de0485bcd15
ericdeback <1lovegithub>
2020-03-20 04:06:23 +0000

    v1.0.1 commit. changed comments
    Signed-off-by: ericdeback <1loveGithub>

--- src/MyApp.java
+++ src/MyApp.java
@@ -15,8 +15,9 @@ public class MyApp {
     // TODO fix this now - OK This is fixed now
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.exit(0);
 
-        // foo
+        new MyApp();
+
+        // foo master
     }
 }
