3d9058ea9569009f3f47cedbd6aaf7e4ab901133
ericdeback <1loveGithub>
2024-04-07 16:55:23 +0100

    updated surefire to V3 so Junit5 tests can be executed

--- src/main/Application/MyApp.java
+++ src/main/Application/MyApp.java
@@ -1,4 +1,4 @@
-package main.java;
+package Application;
 
 public class MyApp {
         public static void main(String[] args) {
---
+++ src/test/java/MyAppTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+public class MyAppTest {
+
+    @Test
+    public void MyAppDumbTest() {
+        assertEquals(1, 1);
+    }
+}
