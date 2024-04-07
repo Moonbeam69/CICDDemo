f93280585cfbedfe12e2320e0177307829e018b8
ericdeback <1loveGithub>
2024-04-07 13:46:23 +0100

    clean up code

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # HelloWorld
-this text file open each time a project is opened in Intellij 
-Uses Apache 2.0 license:
+
 
--- src/MyApp.java
+++ src/MyApp.java
@@ -1,23 +1,5 @@
-import javax.swing.*;
-
 public class MyApp {
-
-    MyApp() {
-        System.out.println("I am a constructor");
-    }
-
-    // feature bla
-    private void setMeUp(JFrame frame) {
-        frame.setVisible(true);
-    }
-
-
-    // TODO fix this now - OK This is fixed now
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("Hello world");
-
-        new MyApp();
-
-        // foo master
     }
 }
