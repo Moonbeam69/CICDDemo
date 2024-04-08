aa9e6db13c842ae22e3e051cc5988de063487639
ericdeback <1loveGithub>
2024-04-08 11:20:32 +0100

    Added a Playwright test case

---
+++ src/test/java/PlaywrightTest.java
@@ -0,0 +1,34 @@
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+import java.util.regex.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.*;
+
+public class PlaywrightTest {
+
+    @Test
+    public void runthistest() {
+        try (Playwright playwright = Playwright.create()) {
+            BrowserType browserType = playwright.chromium();
+            Browser browser = browserType.launch(new BrowserType.LaunchOptions().setHeadless(false));
+            BrowserContext context = browser.newContext();
+
+            Page page = context.newPage();
+            page.navigate("https://playwright.dev");
+
+            // Expect a title "to contain" a substring.
+            assertThat(page).hasTitle(Pattern.compile("Playwright"));
+            System.out.println(page.title());
+
+            page.locator("//html/body/div/div[2]/header/div/div/a").click();
+            assertThat(page).hasTitle("Installation | Playwright");
+
+            System.in.read();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
