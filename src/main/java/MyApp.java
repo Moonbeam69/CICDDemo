339afc3e5e3a2173d6d9d3465d9da8d3beb0b316
Moonbeam69 <eric.deback@btinternet.com>
2024-04-17 14:21:59 +0100

    dummy update for PR

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -1,6 +1,10 @@
 public class MyApp {
 
+    MyApp() {
+        // constructor goes here
+    }
     public static void main(String[] args) {
             System.out.println("CICD_Project-v1.0 was run successfully");
         }
+
 }
