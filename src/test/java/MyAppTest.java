d02505802bd95b2e54c441ab84811fa3fdbc4539
Moonbeam69 <eric.deback@btinternet.com>
2024-04-22 11:27:13 +0100

    created a build step for DevVer and DevInt parameterised environment in Junit test cases

--- src/test/java/MyAppTest.java
+++ src/test/java/MyAppTest.java
@@ -1,24 +1,33 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 public class MyAppTest {
+    static String environment;
 
+    @BeforeAll
+    public static void setup() {
+        environment = System.getProperty("environment");
+    }
     @Test
     public void FirstAppDumbTest() {
+        System.out.println(environment);
         assertEquals(1, 1);
     }
 
     @Test
     public void SecondAppDumbTest() {
+        System.out.println(environment);
         assertEquals(1, 1);
     }
 
     @Test
     public void ThirdAppDumbTest() {
+        System.out.println(environment);
         assertEquals(1, 1);
     }
 
     @Test
     public void FourthAppDumbTest() {
+        System.out.println(environment);
         assertEquals(1, 1);
     }
 }
