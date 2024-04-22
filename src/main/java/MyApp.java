29199a40d9979a17bb4d20d413557c1193da247a
Moonbeam69 <eric.deback@btinternet.com>
2024-04-22 13:29:43 +0100

    added the system parameter "environment" to the output of MyApp class.

--- src/main/java/MyApp.java
+++ src/main/java/MyApp.java
@@ -5,6 +5,8 @@ public class MyApp {
     }
     public static void main(String[] args) {
             System.out.println("CICD_Project-v1.0 was run successfully");
+            System.out.println("in: " + System.getProperty("environment"));
+
         }
 
 }
